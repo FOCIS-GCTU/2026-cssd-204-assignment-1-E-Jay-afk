a73bc8bd5cea1bcb3872b15831c2ec2293c65e35
E.Jay <2425403135@live.gctu.edu.gh>
2026-05-31 22:13:20 +0000

    Implement initials printing in initials.py
    Added a program that prints initials in large stylized letters.

--- initials.py
+++ initials.py
@@ -1,6 +1,35 @@
 
 def main():
-  pass #code goes here
+  pass # File: initials.py
+# Description: Print out my initials in large stylized letters.
+# Assignment Number: 1
+#
+# Name: JIAGGE EMEKOR KOJO
+# STUDENT ID: 2425403135
+# Email: 2425403135@live.gctu.edu.gh
+# Grader: Mr. Augustus Buckman
+#
+# On my honor, Jiagge Emekor Kojo this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+def main():
+    # Prints my initials in large stylized letters.
+
+    print()
+    print("...JEK")
+    print()
+
+    print("JJJJJJJJJJJJ....EEEEEEEEEEEE....K.......K.......")
+    print(".....J..........E...............K.....K.........")
+    print(".....J..........E...............K...K...........")
+    print(".....J..........E...............K.K.............")
+    print(".....J..........EEEEEEEEEEEE....KK..............")
+    print(".....J..........E...............K.K.............")
+    print(".....J..........E...............K...K...........")
+    print("J....J..........E...............K.....K.........")
+    print("J....J......****E...........****K.......K...****")
+    print("..JJJJ......****EEEEEEEEEEEE****K.........K.****")
 
 
 
